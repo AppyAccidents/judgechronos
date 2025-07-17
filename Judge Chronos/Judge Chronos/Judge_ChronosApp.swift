f92d053bb4a40501ec38fb7f5aa606c96c23fc5a
Berker Ceylan <berkerceylan@gmail.com>
2025-07-17 17:57:56 +0300

    Add initial Xcode project (Judge Chronos)

---
+++ Judge Chronos/Judge Chronos/Judge_ChronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Judge_ChronosApp.swift
+//  Judge Chronos
+//
+//  Created by Berker Ceylan on 17.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Judge_ChronosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Judge Chronos/Judge ChronosTests/Judge_ChronosTests.swift
@@ -0,0 +1,17 @@
+//
+//  Judge_ChronosTests.swift
+//  Judge ChronosTests
+//
+//  Created by Berker Ceylan on 17.07.2025.
+//
+
+import Testing
+@testable import Judge_Chronos
+
+struct Judge_ChronosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
